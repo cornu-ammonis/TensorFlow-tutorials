43e54ed4cb7852d0bf73a3b4bcbaad3e204b88bb
cornu-ammonis <andrewjones232@gmail.com>
2017-02-16 21:33:01 -0500

    tests model
    uses arg max and cast to floating point to compute percentage of correct
    responses

--- mnist/mnist_softmax.py
+++ mnist/mnist_softmax.py
@@ -74,6 +74,19 @@ def main(_):
     for _ in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
         sess.run(train_step, feed_dict{x: batch_xs, y_ batch_ys})
+
+
+    #test model after training
+    #argmax(y,1) will give the label our model believes is correct. tf.argmax(y_, 1) will give correct label.
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+    #cast to floating point to get percentages
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels)))
+    
+    
+    
     
     
     
