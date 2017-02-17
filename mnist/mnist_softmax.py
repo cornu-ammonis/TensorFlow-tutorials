258001a49dc3af9cd9ae2463864cd921b1a31703
cornu-ammonis <andrewjones232@gmail.com>
2017-02-16 19:54:35 -0500

    import statement for tensorflow and main method definition

--- mnist/mnist_softmax.py
+++ mnist/mnist_softmax.py
@@ -30,4 +30,8 @@
 
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+import tensorflow as tf
+
+def main(_):
+    
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
