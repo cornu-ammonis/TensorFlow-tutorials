1637f44b8b9cca222f86de5556e949b4c86f384f
cornu-ammonis <andrewjones232@gmail.com>
2017-02-18 18:29:49 -0500

    train and display test results

--- mnist/mnist_softmax.py
+++ mnist/mnist_softmax.py
@@ -105,7 +105,7 @@ def main(_):
     #tf.nn.dropout automaticaly masks and scales neuron outputs so dropout requires no additional scaling
 
     keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep prob)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     # --- READOUT LAYER ---
     W_fc2 = weight_variable([1024, 10])
@@ -131,21 +131,25 @@ def main(_):
 
     #launch model and initializes variables
     sess = tf.InteractiveSession()
-
     sess.run(tf.global_variables_initializer())
 
+    #loop through training and log every 100th iteration in training process
+
+    for i in range(20000):
+        batch = mnist.train.next_batch(50)
+        if i%100 == 0:
+            train_accuracy = accuracy.eval(feed_dict={
+                x:batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %g"%(i, train_accuracy))
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    print("test accuracy %g"%accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+    
+
    
-    #train
-    for _ in range(10000):
-        batch_xs, batch_ys = mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-
-    
-
-    
-
-    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+ 
 
 
 #initialize weights with small amount of noise for symmetry breaking and prevention of 0 gradients
